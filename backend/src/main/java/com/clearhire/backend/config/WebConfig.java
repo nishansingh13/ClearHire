d7607b86654dcfe75bfcf82869489f8a3a106ecb
nishansingh13 <nishansingh2480@gmail.com>
2025-07-11 22:24:26 +0530

    added vercel

--- backend/src/main/java/com/clearhire/backend/config/WebConfig.java
+++ backend/src/main/java/com/clearhire/backend/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://clearhire.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true); // if using cookies
             }
